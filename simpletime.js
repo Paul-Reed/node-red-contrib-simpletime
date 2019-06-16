6ca5fc0d483875fd788897ce4d5436aaaa93a765
Paul <pauldreed@gmail.com>
2019-06-16 20:10:34 +0100

    Add 12hr 'hour' output (#7)
    Also ensure that all msg's are consistently 'strings', instead of numbers/strings.

--- simpletime.js
+++ simpletime.js
@@ -5,6 +5,7 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
 const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const dayNames =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            
 var d = new Date();
         dts = d.toDateString();
         e = d.getTime();
@@ -20,32 +21,38 @@ var d = new Date();
         h = pad(hr, 2);
         m = pad(mi, 2);
         s = pad(d.getSeconds(), 2);
+       
+        // Get hour in 12hr format 
+        if (hr===0){thr=12;}
+            else if (hr>12){thr=hr-12;}
+                else {thr=hr;}
+                thr='' + thr;
 
+        // Calculate if AM or PM
         var amp = ((hr*60)+mi);
-            if (amp<720) {
-                amp="AM";
-            } else {amp="PM";}
+            if (amp<720){amp="AM";}
+                else {amp="PM";}
         
         var hm = (h+":"+m);
         var hms = (h+":"+m+":"+s);
         var ms = (m+":"+s);
         
         msg.mydate = dts;
-        msg.myyear = yr;
+        msg.myyear = ''+yr;
         msg.mymonth = mn;
         msg.mymonthn = mnu;
         msg.mydom = dt;
         msg.myday = dy;
+        msg.myhourpm = thr;
         msg.myhour = h;
         msg.mytime = hm;
         msg.mytimes = hms;
         msg.myminute = m;
         msg.myminutes = ms;
         msg.mysecond = s;
-        msg.myepoch = e;
+        msg.myepoch = ''+e;
         msg.myrawdate = d;
         msg.mypm = amp;
-
         node.send(msg);
         });
     }
